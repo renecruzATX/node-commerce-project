ab67c110c0f64ca79d1e14a95e045b7ae21d411c
Rene Cruz <renecruzATX@outlook.com>
2019-02-06 15:28:09 -0600

    adding gitignore

--- routes.js
+++ routes.js
@@ -7,10 +7,10 @@ const requestHandler = (req, res) => {
     if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');    
     res.write('</html>');
     return res.end();
-  }
+  };
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
