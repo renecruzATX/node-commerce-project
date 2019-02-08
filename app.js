97a41fdd11dd351dbf8c70b4ec92244c05f7ea94
Rene Cruz <renecruzATX@outlook.com>
2019-02-08 00:53:26 -0600

    Added Express and html pages

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3006);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3006);
