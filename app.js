f3688dd75ed15c2d1a041746687cfb77442e3fd8
Rene Cruz <renecruzATX@outlook.com>
2019-07-09 14:51:52 -0500

    Added a MySQL db and added the functionality to retrieve from that db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -22,3 +23,4 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3006);
+
