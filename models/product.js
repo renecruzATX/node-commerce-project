86e909c430fab2b57087787f41d5c18ce1466b85
Rene Cruz <renecruzATX@outlook.com>
2019-07-09 17:38:34 -0500

    Added functionality for inserting items into db and fetching a single product

--- models/product.js
+++ models/product.js
@@ -12,7 +12,9 @@ module.exports = class Product {
     }
 
     save () {         
-        
+        return db.execute(
+            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description]
+        );
     }
 
     static deleteById(id) {
@@ -20,11 +22,11 @@ module.exports = class Product {
       }
 
     static fetchAll() {
-       return db.execute('SELECT * FROM products');
+        return db.execute('SELECT * FROM products');
     }
 
     static findById(id) {
-               
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);        
     }
 };
 
